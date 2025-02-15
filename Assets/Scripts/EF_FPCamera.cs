25d8205708fb31504e486d038b53c5b8c020b5c2
Eiryn <eiryn@live.co.uk>
2025-02-15 20:10:54 +0000

    simpler camera rotation

---
+++ Assets/Scripts/EF_FPCamera.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF_FPCamera : MonoBehaviour
+{
+    private GameObject target;
+
+    public Transform player;
+    public float mouseSensitivity = 2.0f;
+    float camVerticleRot = 0f;
+
+    bool lockCursor = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        //Lock Cursor to middle of screen
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves Camera with Player
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 0.8f, target.transform.position.z);
+
+        //Mouse Input
+        float inputX = Input.GetAxis("Mouse X")*mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y")*mouseSensitivity;
+
+        //Rotate Camera x axis
+
+        camVerticleRot -= inputY;
+        camVerticleRot = Mathf.Clamp(camVerticleRot, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * camVerticleRot;
+
+        //Rotate Player and Camera y axis
+
+        player.Rotate(Vector3.up * inputX);
+    }
+}
