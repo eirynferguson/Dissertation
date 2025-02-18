7f1328e7ce69414a91beeaf5ae60aa68b0967309
Eiryn <eiryn@live.co.uk>
2025-02-18 19:20:43 +0000

    scene changer script and buttons usable

---
+++ Assets/Scripts/EF_SceneChanger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EF_SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //function used to switch between scenes
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
