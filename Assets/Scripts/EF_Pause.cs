76a08e711871c7c1b85a02353096e4d4fb7d319d
Eiryn <eiryn@live.co.uk>
2025-02-18 21:15:46 +0000

    pause menu and script incomplete

---
+++ Assets/Scripts/EF_Pause.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EF_Pause : MonoBehaviour
+{
+    public GameObject buttons;
+    public GameObject pauseUI;
+    public GameObject controlsButton;
+
+    public static bool isPaused = false;
+    public static bool controlsShown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlsShown = false;
+        isPaused = false;
+        buttons.SetActive(false);
+        controlsButton.SetActive(false);
+        ResumeGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Paused");
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+
+        buttons.SetActive(true);
+        pauseUI.SetActive(true);
+        isPaused = true;
+
+        controlsButton.SetActive(false);
+        controlsShown = false;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Unpaused");
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+
+        buttons.SetActive(false);
+        pauseUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void controls()
+    {
+        Time.timeScale = 0f;
+        controlsButton.SetActive(true);
+        controlsShown = true;
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
